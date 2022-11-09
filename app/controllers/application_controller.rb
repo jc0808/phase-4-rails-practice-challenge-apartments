a0dab9676737094deedb4ea1b8e23378de932e05
Joshua Campos <jdeveloper1203@gmail.com>
2022-11-09 16:55:20 -0500

    commit tree 187614cb6be92c5ea3bf03acb02c7b91cbfea730

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_message
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_message
+
+
+    private
+
+    def render_not_found_message(exception)
+        render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
+
+    def render_invalid_message(exception)
+        render json: {errors: "#{exception.record.errors.full_message}"}, status: :unprocessable_entity
+    end
 end
